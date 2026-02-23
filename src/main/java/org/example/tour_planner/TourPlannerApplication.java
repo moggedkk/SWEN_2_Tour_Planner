c0908aa989fda0a927975e42fb87103541960c23
Abris Mezo <abris.mezo@gmail.com>
2026-02-23 15:16:20 +0100

    added spring boot

---
+++ src/main/java/org/example/tour_planner/TourPlannerApplication.java
@@ -0,0 +1,13 @@
+package org.example.tour_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourPlannerApplication.class, args);
+    }
+
+}
