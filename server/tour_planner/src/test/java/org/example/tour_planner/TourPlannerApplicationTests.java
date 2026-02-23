e0e0fda19f2682befe43dd9645aad62ab9208e7e
Abris Mezo <abris.mezo@gmail.com>
2026-02-23 15:42:30 +0100

    Convert server/tour_planner from submodule to regular directory

---
+++ server/tour_planner/src/test/java/org/example/tour_planner/TourPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tour_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
