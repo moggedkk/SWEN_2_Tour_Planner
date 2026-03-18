f39e5960551ecdcc5142dbd226b62415cc4bfc94
Adrian Strassnig <adrianstrassnig2@gmail.com>
2026-03-18 21:51:27 +0100

    Added content

--- ui/src/app/main/main.ts
+++ ui/src/app/main/main.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
-import { Navbar } from '../navbar/navbar'
-import { RouterOutlet } from '@angular/router';
+import { Navbar } from '../navbar/navbar';
+import { AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-main',
-  imports: [Navbar, RouterOutlet],
+  imports: [Navbar],
   standalone: true,
   templateUrl: './main.html',
   styleUrl: './main.css',
 })
-export class Main {
+export class Main implements AfterViewInit{
+    
+ private map: any = null;
+
+  async ngAfterViewInit() {
+    if (typeof window === 'undefined') return;
+
+    const L = await import('leaflet'); // ✅ dynamic import
+
+    if (this.map) return;
+
+    this.map = L.map('map', {
+      zoomControl: true,
+      attributionControl: true,
+    });
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: "© OpenStreetMap contributors"
+    }).addTo(this.map);
+
+    this.map.setView([48.2082, 16.3738], 12); // Vienna
+  }
 
 }
--- ui/src/app/profile/profile.ts
+++ ui/src/app/profile/profile.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
-import { Navbar } from '../navbar/navbar'
+import { Navbar } from '../navbar/navbar';
+import { CommonModule } from '@angular/common';
+
+interface Tour {
+  name: string;
+  start: string;
+  end: string;
+}
 
 @Component({
   selector: 'app-profile',
-  imports: [Navbar],
   standalone: true,
+  imports: [Navbar, CommonModule],
   templateUrl: './profile.html',
-  styleUrl: './profile.css',
+  styleUrls: ['./profile.css'],
 })
 export class Profile {
+
+  completedTours: Tour[] = [
+    { name: 'Vienna City Tour', start: 'Vienna', end: 'Vienna' },
+    { name: 'Graz Trip', start: 'Vienna', end: 'Graz' }
+  ];
+
+  createdTours: Tour[] = [
+    { name: 'Alps Tour', start: 'Salzburg', end: 'Innsbruck' },
+    { name: 'Danube Route', start: 'Linz', end: 'Vienna' }
+  ];
 
 }
