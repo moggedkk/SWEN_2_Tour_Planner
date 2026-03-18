8e9fb886ab2d82f468def854af03ca4a8207d1b2
Abris Mezo <abris.mezo@gmail.com>
2026-03-18 12:54:14 +0100

    register and login ui basic done

---
+++ ui/src/app/navbar/navbar.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterLink],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css',
+  standalone : true,
+})
+export class Navbar {
+
+}
