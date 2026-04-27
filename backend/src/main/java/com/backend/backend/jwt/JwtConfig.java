6e789fcd1b998798169fd50e5b6e7bb43bacde7e
Adrian Strassnig <adrianstrassnig2@gmail.com>
2026-04-27 23:25:03 +0200

    Added java-jwt libary - implemented createJWT and verifyJWT functions

---
+++ backend/src/main/java/com/backend/backend/jwt/JwtConfig.java
@@ -0,0 +1,41 @@
+package com.backend.backend.jwt;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtConfig {
+    private final String secretKey = "7f9a8c2d4e6b1a9f"; // Should be savely stored, gonna do later
+    public String createJWT(){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secretKey);
+            return JWT.create()
+                    .withIssuer("my-app")
+                    .withSubject("username")
+                    .withIssuedAt(new Date())
+                    .withExpiresAt(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1h
+                    .sign(algorithm);
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Error creating JWT", exception);
+        }
+    }
+    public DecodedJWT verifyJWT(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secretKey);
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer("my-app")
+                    .build();
+
+             return verifier.verify(token); // decoded JWT
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Invalid JWT", exception);
+        }
+    }
+}
