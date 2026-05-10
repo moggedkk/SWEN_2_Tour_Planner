7e0b437e00de39c379d8ba402a4716427dad6d0c
Abris Mezo <abris.mezo@gmail.com>
2026-05-10 12:00:33 +0200

    userservice interface

--- backend/src/main/java/com/backend/backend/service/declaration/IUserService.java
+++ backend/src/main/java/com/backend/backend/service/declaration/IUserService.java
@@ -1,9 +1,12 @@
 package com.backend.backend.service.declaration;
 
+import com.backend.backend.model.dto.RegisterRequest;
 import com.backend.backend.model.entity.User;
 
+import java.util.Optional;
+
 public interface IUserService {
-    User registerUser(User user);
-    User findUserByUsername(String username);
+    User registerUser(RegisterRequest request);
+    Optional<User> findUserByUsername(String username);
     void deleteUser(User user);
 }
