abe6784751250eb4f0a077cb0ea8c79a4fa88771
Abris Mezo <abris.mezo@gmail.com>
2026-05-10 12:06:55 +0200

    made a new private secret key for jwt

--- backend/src/main/java/com/backend/backend/security/jwt/JwtConfig.java
+++ backend/src/main/java/com/backend/backend/security/jwt/JwtConfig.java
@@ -6,13 +6,17 @@ import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
 @Component
 public class JwtConfig {
-    private final String secretKey = "7f9a8c2d4e6b1a9f3d2e5c8b0a4f7e1d"; // Should be safely stored, gonna do later
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
     public String createJWT(String username){
         try {
             Algorithm algorithm = Algorithm.HMAC256(secretKey);
