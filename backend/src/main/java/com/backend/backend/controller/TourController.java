d2e0e8b8a338b78bce9e897316e9ace3b0da6688
Abris Mezo <abris.mezo@gmail.com>
2026-06-14 20:57:10 +0200

    added log4j to pom.xml and wrote a simple config  + started logging

--- backend/src/main/java/com/backend/backend/controller/TourController.java
+++ backend/src/main/java/com/backend/backend/controller/TourController.java
@@ -3,6 +3,7 @@ package com.backend.backend.controller;
 import com.backend.backend.model.dto.TourRequest;
 import com.backend.backend.model.dto.TourResponse;
 import com.backend.backend.service.declaration.ITourService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/tours")
 @CrossOrigin
@@ -24,30 +26,35 @@ public class TourController {
     @PostMapping
     public ResponseEntity<TourResponse> create(@RequestBody TourRequest request) {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        log.info("User '{}' creating tour '{}'", username, request.getName());
         return ResponseEntity.status(HttpStatus.CREATED).body(tourService.createTour(request, username));
     }
 
     @GetMapping
     public ResponseEntity<List<TourResponse>> getAll() {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        log.debug("User '{}' fetching all tours", username);
         return ResponseEntity.ok(tourService.getAllTours(username));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<TourResponse> getOne(@PathVariable int id) {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        log.debug("User '{}' fetching tour id={}", username, id);
         return ResponseEntity.ok(tourService.getTour(id, username));
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<TourResponse> update(@PathVariable int id, @RequestBody TourRequest request) {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        log.info("User '{}' updating tour id={}", username, id);
         return ResponseEntity.ok(tourService.updateTour(id, request, username));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable int id) {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        log.info("User '{}' deleting tour id={}", username, id);
         tourService.deleteTour(id, username);
         return ResponseEntity.noContent().build();
     }
--- backend/src/main/java/com/backend/backend/controller/UserController.java
+++ backend/src/main/java/com/backend/backend/controller/UserController.java
@@ -5,10 +5,12 @@ import com.backend.backend.model.dto.TokenResponse;
 import com.backend.backend.model.entity.User;
 import com.backend.backend.security.jwt.JwtConfig;
 import com.backend.backend.service.declaration.IUserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/users")
 @CrossOrigin
@@ -24,8 +26,10 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<TokenResponse> register(@RequestBody RegisterRequest request) {
+        log.info("Registration attempt for username '{}'", request.getUsername());
         User user = userService.registerUser(request);
         String token = jwtConfig.createJWT(user.getUsername());
+        log.info("User '{}' registered successfully", user.getUsername());
         return ResponseEntity.status(HttpStatus.CREATED).body(new TokenResponse(token));
     }
 }
